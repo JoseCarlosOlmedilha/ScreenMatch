f49c811b5e227930f73a990ed879dd93ffb9a6d1
JoseCarlosOlmedilha <jose.olmedilha@outlook.com>
2025-12-07 23:38:37 -0300

    Aprendendo e implementando sobrescrita

--- src/br/com/screenmatch/aula/main.java
+++ src/br/com/screenmatch/aula/main.java
@@ -1,8 +1,35 @@
 package br.com.screenmatch.aula;
 
+import br.com.screenmatch.aula.modelos.Filme;
+import br.com.screenmatch.aula.modelos.Serie;
+
 public class main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLançamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos() + " minutos");
+
+        meuFilme.exibirFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDasAvaliacoes());
+        System.out.println(meuFilme.getMediaAvaliacoes());
 
 
+
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLançamento(2000);
+        lost.exibirFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar lost: " + lost.getDuracaoEmMinutos() + " minutos");
     }
 }
--- src/br/com/screenmatch/aula/modelos/Serie.java
+++ src/br/com/screenmatch/aula/modelos/Serie.java
@@ -38,4 +38,15 @@ public class Serie extends Titulo{
     public void setMinutosPorEpisodio(int minutosPorEpisodio) {
         this.minutosPorEpisodio = minutosPorEpisodio;
     }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodioPorTemporada * minutosPorEpisodio;
+    }
+
+
+
+
+
+
 }
